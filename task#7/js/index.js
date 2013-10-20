08755ebcc72389aaa8752495f85b0ad2822c3729
alekseyleshko <alekseyleshko@gmail.com>
2013-10-21 00:40:52 +0400

    finish task#7

--- task#7/js/index.js
+++ task#7/js/index.js
@@ -1,27 +1,100 @@
-var Controller = Backbone.Router.extend({
-    routes: {
-        "": "start", // Пустой hash-тэг
-        "!/": "start", // Начальная страница
-        "!/success": "success", // Блок удачи
-        "!/error": "error" // Блок ошибки
-    },
+$(function () {
+    var AppState = Backbone.Model.extend({
+        defaults: {
+            username: "",
+            state: "start"
+        }
 
-    start: function () {
-        $(".block").hide(); // Прячем все блоки
-        $("#start").show(); // Показываем нужный
-    },
+    });
 
-    success: function () {
-        $(".block").hide();
-        $("#success").show();
-    },
+    var UserNameModel = Backbone.Model.extend({
+        defaults: {
+            "Name": ""
+        }
+    });
 
-    error: function () {
-        $(".block").hide();
-        $("#error").show();
-    }
+    var Family = Backbone.Collection.extend({
+        model: UserNameModel,
+
+        checkUser: function (username) {
+            var findResult = this.find(function (user) {
+                return user.get("Name").toLowerCase() == username.toLowerCase()
+            });
+            return findResult != null;
+        }
+    });
+
+    var MyFamily = new Family([
+                { Name: "Aleksey" },
+                { Name: "Test" }
+            ]);
+
+    var Block = Backbone.View.extend({
+        el: $("#block"),
+
+        templates: {
+            "start": _.template($('#start').html()),
+            "success": _.template($('#success').html()),
+            "error": _.template($('#error').html())
+        },
+
+        initialize: function () {
+            this.model.bind('change', this.render, this);
+        },
+
+        render: function () {
+            var state = this.model.get("state");
+            $(this.el).html(this.templates[state](this.model.toJSON()));
+            return this;
+        },
+
+        events: {
+            "click input:button": "check"
+        },
+
+        check: function () {
+            var username = this.el.find("input:text").val();
+            var find = MyFamily.checkUser(username);
+            appState.set({
+                "state": find ? "success" : "error",
+                "username": username
+            });
+        },
+    });
+
+    var Controller = Backbone.Router.extend({
+        routes: {
+            "": "start",
+            "!/": "start",
+            "!/success": "success",
+            "!/error": "error"
+        },
+
+        start: function () {
+            appState.set({ state: "start" });
+        },
+
+        success: function () {
+            appState.set({ state: "success" });
+        },
+
+        error: function () {
+            appState.set({ state: "error" });
+        }
+    });
+
+    var appState = new AppState();
+
+    var controller = new Controller();
+    var block = new Block({ model: appState });
+    appState.trigger("change");
+
+    appState.bind("change:state", function () {
+        var state = this.get("state");
+        if (state == "start")
+            controller.navigate("!/", false);
+        else
+            controller.navigate("!/" + state, false);
+    });
+    Backbone.history.start();
 });
-
-var controller = new Controller(); // Создаём контроллер
-
-Backbone.history.start();  // Запускаем HTML5 History push    
